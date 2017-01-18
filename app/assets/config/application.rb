2b26decbf37572865482eaab087ce39b7ced5f92
dhakle <carlitos_alm@hotmail.com>
2017-01-18 16:08:15 -0500

    added puma to productions

--- app/assets/config/application.rb
+++ app/assets/config/application.rb
@@ -1,7 +1,4 @@
-config.assets.paths << Rails.root.join("app", "assets", "stylesheets")
-config.assets.paths << Rails.root.join("app", "assets", "javascripts")
-config.assets.paths << Rails.root.join("vendor", "assets", "stylesheets")
-config.assets.paths << Rails.root.join("vendor", "assets", "javascripts", "fonts")
+
 # config.serve_static_assets = true
 # config.assets.initialize_on_precompile = false
 
