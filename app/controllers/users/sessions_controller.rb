052d7082bc8fb3ea0cfe8f784b55a6006570c9f3
dhakle <carlitos_alm@hotmail.com>
2017-01-18 16:28:24 -0500

    sessions conttrolled added module users

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,3 +1,4 @@
+module Users
 class Users::SessionsController < DeviseController
   prepend_before_action :require_no_authentication, only: [:new, :create]
   prepend_before_action :allow_params_authentication!, only: :create
@@ -78,4 +79,5 @@ class Users::SessionsController < DeviseController
       format.any(*navigational_formats) { redirect_to after_sign_out_path_for(resource_name) }
     end
   end
+end
 end
