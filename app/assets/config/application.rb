3fd2f7050f5d08372979b90adfcda9b132c4e78c
dhakle <carlitos_alm@hotmail.com>
2017-01-18 15:53:23 -0500

    commeneted lines app.rb

--- app/assets/config/application.rb
+++ app/assets/config/application.rb
@@ -2,7 +2,7 @@ config.assets.paths << Rails.root.join("app", "assets", "stylesheets")
 config.assets.paths << Rails.root.join("app", "assets", "javascripts")
 config.assets.paths << Rails.root.join("vendor", "assets", "stylesheets")
 config.assets.paths << Rails.root.join("vendor", "assets", "javascripts", "fonts")
-config.serve_static_assets = true
-config.assets.initialize_on_precompile = false
+# config.serve_static_assets = true
+# config.assets.initialize_on_precompile = false
 
 require 'rails/all'
