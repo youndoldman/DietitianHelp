80442fba6f67705475ae8ad51902c0f94ec3dd65
dhakle <carlitos_alm@hotmail.com>
2017-01-19 22:25:56 -0500

    assets application rb default

--- app/assets/config/application.rb
+++ app/assets/config/application.rb
@@ -1,6 +1,3 @@
-config.serve_static_assets = true
-config.assets.initialize_on_precompile = false
-
 config.assets.paths << Rails.root.join("app", "assets", "stylesheets")
 config.assets.paths << Rails.root.join("app", "assets", "javascripts")
 config.assets.paths << Rails.root.join("vendor", "assets", "stylesheets")
