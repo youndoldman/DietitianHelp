1d4af1e40757a9f99de1df90024ce9db25fa9951
dhakle <carlitos_alm@hotmail.com>
2017-01-29 21:26:10 -0500

    aa

--- app/assets/config/application.rb
+++ app/assets/config/application.rb
@@ -1,4 +1,3 @@
-Rails.application.configure do
 config.serve_static_assets = true
 config.assets.initialize_on_precompile = false
 
@@ -8,7 +7,7 @@ config.assets.paths << Rails.root.join("vendor", "assets", "stylesheets")
 config.assets.paths << Rails.root.join("vendor", "assets", "javascripts")
 
 require 'rails/all'
-end
+
 
 
 rails g scaffold Client firstname:string lastname:string allergies:string cdiet:string dx:string ht:integer cbw:integer date0:string thirtywt:integer ninetywt:integer oneeightywt:integer date1:string date2:string date3:strin intakefrom:integer intaketo:integer bmi:decimal ibw:integer calreq:integer proreq:integer flreq:integer fassess:string fpes:string pes0:string pes1:string pes2:string
