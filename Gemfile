503ad44c07e7738d97671c430222cca582d19541
dhakle <carlitos_alm@hotmail.com>
2017-03-19 08:45:09 -0400

    Title update

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 ruby '2.2.3'
 
-
+gem 'rename'
 gem 'devise'
 # AUTOCOMPLETE
 # gem "selectize-rails"
--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Yellow
+module DietitianHelp
   class Application < Rails::Application
   	config.action_view.embed_authenticity_token_in_remote_forms = true
     # Settings in config/environments/* take precedence over those specified here.
