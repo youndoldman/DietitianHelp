bbe03e8a6e8acab39f66d057c0efe860213023bc
dhakle <carlitos_alm@hotmail.com>
2017-04-11 08:54:28 -0400

    fix history db 8

--- db/migrate/20170411122016_rename_refs_in_doc.rb
+++
@@ -1,7 +0,0 @@
-class RenameRefsInDoc < ActiveRecord::Migration[5.0]
-  def change
-  	rename_column :documents, :fullassessment_id_id, :fullassessment_id
-  	rename_column :documents, :progressnote_id_id, :progressnote_id
-  	rename_column :documents, :monitoringnote_id_id, :monitoringnote_id
-  end
-end
