8271bc193f472ce3c3414e965134e9d772dd9812
dhakle <carlitos_alm@hotmail.com>
2017-04-11 08:51:56 -0400

    fix history db 7

--- db/migrate/20170410135457_create_documents.rb
+++ db/migrate/20170410135457_create_documents.rb
@@ -2,9 +2,9 @@ class CreateDocuments < ActiveRecord::Migration[5.0]
   def change
     create_table :documents do |t|
       t.belongs_to :client, foreign_key: true
-      t.references :fullassessment, foreign_key: true
-      t.references :progressnote, foreign_key: true
-      t.references :monitoringnote, foreign_key: true
+      t.references :fullassessment_id, foreign_key: true
+      t.references :progressnote_id, foreign_key: true
+      t.references :monitoringnote_id, foreign_key: true
 
       t.timestamps
     end
