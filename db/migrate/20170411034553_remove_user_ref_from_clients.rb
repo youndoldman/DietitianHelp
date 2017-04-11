3ee39be26e7b51111132585c787a1963c9f0a5d0
dhakle <carlitos_alm@hotmail.com>
2017-04-11 08:36:14 -0400

    fixing history db 5

--- db/migrate/20170411034553_remove_user_ref_from_clients.rb
+++ db/migrate/20170411034553_remove_user_ref_from_clients.rb
@@ -1,5 +1,5 @@
 class RemoveUserRefFromClients < ActiveRecord::Migration[5.0]
   def change
-    remove_reference :clients, :user_id, foreign_key: true
+    remove_reference :clients, :user, foreign_key: true
   end
 end
