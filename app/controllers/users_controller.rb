564f17a13c3c86c971ad2a921e57516f09e57875
dhakle <carlitos_alm@hotmail.com>
2017-05-14 00:59:18 -0400

    logger tester

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-    puts "I RAAAAAN!!"
+
     respond_to do |format|
       if @user.save
         format.html { redirect_to root_url, :notice => "" }
