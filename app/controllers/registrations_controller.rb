bfcd1b2c7855277ea2b814b0b027507eac2f499e
dhakle <carlitos_alm@hotmail.com>
2017-05-14 00:09:28 -0400

    reg controller commented out

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,31 +1,32 @@
-class RegistrationsController < Devise::RegistrationsController  
-skip_before_filter :verify_authenticity_token, :only => :create
-class RegistrationsController < Devise::RegistrationsController
+# class RegistrationsController < Devise::RegistrationsController  
+# skip_before_filter :verify_authenticity_token, :only => :create
+# class RegistrationsController < Devise::RegistrationsController
  
-  def create
-    build_resource
+#   def create
+#     build_resource
  
-    if resource.save
-      if resource.active_for_authentication?
-        set_flash_message :notice, :signed_up if is_navigational_format?
-        sign_up(resource_name, resource)
-        return render :json => {:success => true}
-      else
-        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
-        expire_session_data_after_sign_in!
-        return render :json => {:success => true}
-      end
-    else
-      clean_up_passwords resource
-      return render :json => {:success => false}
-    end
-  end
+#     if resource.save
+#       puts "I RAAAAN"
+#       if resource.active_for_authentication?
+#         set_flash_message :notice, :signed_up if is_navigational_format?
+#         sign_up(resource_name, resource)
+#         return render :json => {:success => true}
+#       else
+#         set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
+#         expire_session_data_after_sign_in!
+#         return render :json => {:success => true}
+#       end
+#     else
+#       clean_up_passwords resource
+#       return render :json => {:success => false}
+#     end
+#   end
  
-  # Signs in a user on sign up. You can overwrite this method in your own
-  # RegistrationsController.
-  def sign_up(resource_name, resource)
-    sign_in(resource_name, resource)
-  end
+#   # Signs in a user on sign up. You can overwrite this method in your own
+#   # RegistrationsController.
+#   def sign_up(resource_name, resource)
+#     sign_in(resource_name, resource)
+#   end
  
-end
-end
+# end
+# end
